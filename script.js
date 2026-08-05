b451c8e01ae9253e3e251fac98e980567885fbe8
nic <your@email.com>
2026-08-05 20:12:15 +0530

    Add contact section with social buttons

---
+++ script.js
@@ -0,0 +1,11 @@
+new Typed("#element", {
+    strings: [
+        "Front-End Developer",
+        "Game Developer",
+        "Linux Enthusiast"
+    ],
+    typeSpeed: 70,
+    backSpeed: 40,
+    backDelay: 1500,
+    loop: true
+});
